7339739e995c694899daaa17f286f99015428d5f
juhitiwari <tjuhi.321@gmail.com>
2018-05-22 22:24:51 +0530

    Initial commit

---
+++ sent.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 19:47:53 2018
+
+@author: slytherin
+"""
+
+import sentiment_mod as s
+
+print(s.sentiment("Concepts were wonderfully explained! AASF is awesome"))
+print(s.sentiment("Such a boring blog. Try for better content"))
+
